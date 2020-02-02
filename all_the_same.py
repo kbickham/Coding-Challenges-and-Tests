f4ee9876b83c5626147fceeacac6f228356cc59c
Kris Bickham <krisbickham@gmail.com>
2020-02-01 19:53:51 -0600

    Delete all_the_same.py

--- all_the_same.py
+++
@@ -1,9 +0,0 @@
-    if len(elements) == 0:
-        return True    
-    else:
-        if elements.count(elements[0]) == len(elements):
-             return True
-        else:
-             return False
-     
-
