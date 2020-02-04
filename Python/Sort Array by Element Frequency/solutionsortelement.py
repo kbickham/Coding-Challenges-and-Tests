77e97016b3c831b9ab5dc556f2506254f5eabbd4
Kris Bickham <krisbickham@gmail.com>
2020-02-04 14:15:48 -0600

    https://py.checkio.org/en/mission/sort-array-by-element-frequency/
    I feel like my argument ...count - the index ...then reverse , may break under certain conditions? I still need to understand this sorted function better

---
+++ Python/Sort Array by Element Frequency/solutionsortelement.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+def frequency_sort(items):
+    count = Counter(items)
+    return sorted(sorted(items),key=lambda x: count[x] - items.index(x) ,reverse = True)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4])) == [4, 4, 4, 4, 6, 6, 2, 2]
+    assert list(frequency_sort(['bob', 'bob', 'carl', 'alex', 'bob'])) == ['bob', 'bob', 'bob', 'carl', 'alex']
+    assert list(frequency_sort([17, 99, 42])) == [17, 99, 42]
+    assert list(frequency_sort([])) == []
+    assert list(frequency_sort([1])) == [1]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
