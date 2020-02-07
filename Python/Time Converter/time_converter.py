687dd8ad5988d72f38a6ffe1a4ab69defd40f667
Kris Bickham <krisbickham@gmail.com>
2020-02-06 22:16:17 -0600

    https://py.checkio.org/en/mission/time-converter-24h-to-12h/

---
+++ Python/Time Converter/time_converter.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+def time_converter(time):
+    timestr2=[chars.replace("P","p.").replace("A","a.").replace("M","m.") for chars in list(datetime.strptime(time,'%H:%M').strftime("%-I:%M %p"))] 
+    return ''.join(timestr2)
+
+if __name__ == '__main__':
+    print("Example:")
+    print(time_converter('12:30'))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert time_converter('12:30') == '12:30 p.m.'
+    assert time_converter('09:00') == '9:00 a.m.'
+    assert time_converter('23:15') == '11:15 p.m.'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
